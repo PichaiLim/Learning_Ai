14c7db72c9fa833dca1e1d19a35f1e05b8814732
pichai-lab <pichai@jitinnotech.com>
2026-02-05 18:06:21 +0700

    create python get all file at folder

---
+++ Retrieval_Augmented_Generation_RAG/load_document.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# ============================================================================
+# TODO: LOAD DOCUMENT
+# 1) Get document all in folder
+# 2) Using pdf2image to convert pdf to image
+# 3) Using img2pdf to convert image to pdf
+# ============================================================================
+class LoadDocument:
+    def __init__(self):
+        self.base_dir = os.path.dirname(__file__)
+        self.pdf_path = os.path.join(self.base_dir, 'media', 'PDPA_thailand.pdf')
+        self.output_path = os.path.join(self.base_dir, 'media/output')
+
+    def documetn_in_folder(self, folder_path, file_name):
+        for file in os.listdir(folder_path):
+            if not file.endswith(file_name):
+                continue
+            return file
+        return None
+
+    def convert_pdf_to_image(self, pdf_path, output_path, dpi=300, orientation='portrait', image_format='JPEG'):
+        images = convert_from_path(pdf_path, dpi=dpi, orientation=orientation)
+        for i, image in enumerate(images):
+            image.save(f'{output_path}/images/page_{i+1}.{image_format}', image_format)
+
+if __name__ == '__main__':
+    load_document = LoadDocument()
+    print(load_document.documetn_in_folder(load_document.base_dir, 'PDPA_thailand.pdf'))
